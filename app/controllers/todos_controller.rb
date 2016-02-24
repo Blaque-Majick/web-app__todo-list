de0a74ed22fcffdfcd385107f0efc5a65d33ed02
Tray Harrington <traylon.harrington@gmail.com>
2016-02-24 15:13:51 -0600

    Create for Todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,1 +1,13 @@
 # This controller is for all the CRUD operations related to a Todo.
+
+MyApp.get "/new_todo_form" do
+  erb :"/todos/new_todo_form"
+end
+
+MyApp.post "/new_todo" do
+  @todo = Todo.new
+  @todo.title = params["title"]
+  @todo.description = params["description"]
+  @todo.save
+  erb :"/todos/create_success"
+end
